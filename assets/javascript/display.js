e3421320b780daec4f85658fe3af560b47346772
unknown <0803003@nchc.org.tw>
2019-07-08 10:42:30 +0800

    first version

---
+++ assets/javascript/display.js
@@ -0,0 +1,282 @@
+"use strict";
+
+// All visible changes to the page
+const display = {
+	// Animation properties
+	shakeTime: 150,  // Duration of shake animation
+	hiScoreTime: 80,  // Timeout delay for each new shown high score row
+	flashOn: true,  // Screen flash on word miss
+
+
+	// Methods
+	startGame: function(){},
+		// Fades out game options and starts game
+		// Calls: game.start
+
+	gameOver: function(){},
+		// Show game over text
+
+	showOptions: function(){},
+		// Shows word list selector buttons, modifies #output div css
+
+	loginComplete: function(){},
+		// Fades out initial login text/buttons
+		// Calls: game.chooseOptions
+
+	changeTheme: function(){},
+		// Modifies background and other display properties based on selected game difficulty
+
+	addWord: function(word){},
+		// Places a new falling word on screen, sets a timeout to call removeWord if not completed in game.wordSpeed
+		// Calls: game.removeWord
+
+	removeWord: function(word){},
+		// Removes falling word from screen, triggers game-body flash animation
+
+	updateWord: function(word){},
+		// Changes letter color to red and space to underscore
+
+	resetWord: function(word){},
+		// Resets word HTML to starting state
+
+	blowUpWord: function(word){},
+		// Play word explosion animation
+
+	shakeWord: function(word){},
+		// Play word shake animation
+
+	stats: function(){},
+		// Shows current game stats below game
+
+	highScores: function(arr, index){}
+		// Shows high score table data, where index is the array index of the most recently added score
+		// row[index] will be faded in; if (index === null) every row will fade in
+};
+
+
+// Method definitions
+Object.defineProperties(display, {
+	"startGame": { value: function() {
+		$("#chooseGame").fadeOut(function() {
+			$("#output").removeClass("flex");
+			$("#output").empty();
+			if (!game.ready) { game.start(); }
+			$("#score-bar").css("visibility", "visible");
+			$("#" + game.currentDifficulty + "-list").click();
+		});
+	}},
+
+	"gameOver": { value: function() {
+		const mins = Math.floor(Math.floor(stats.duration) / 60);
+		const secs = Math.floor(stats.duration) % 60;
+
+		const template = "\
+			<div>\
+				<h1>GAME OVER</h1>\
+			</div>\
+			<br/><br/>\
+			<div class='over-text'>\
+				<h3>Final Score: &nbsp;" + stats.score + "</h3><br/>\
+				<h3>You Lasted: &nbsp;" + mins + " min, " + secs + " sec</h3><br/>\
+				<h3>Accuracy: &nbsp;" + stats.hits + " correct / " + (stats.hits + stats.misses) + " total ( " + stats.acc.toFixed(1) + "% )</h3><br/>\
+				<h3>Longest Word Completed: &nbsp;<i>" + (stats.longestWord || "(none)") + "</i></h3>\
+			</div>\
+			<br/><br/>\
+			<div>\
+				<button id='playAgain' class='btn btn-primary'>Play Again?</button>\
+			</div>";
+
+		$("#output").addClass("flex").html($("<div>").addClass("text-center").attr("id", "thanks").html(template).fadeIn());
+		$("#score-mult").empty();
+	}},
+
+	"showOptions": { value: function() {
+		const template = "\
+			<div>\
+				<div id='big-source'>\
+					<span id='source-text'>bacon</span>\
+					<select>\
+						<option value='+47'>Norge (+47)</option>\
+						<option value='+46'>Sverige (+46)</option>\
+						<option value='+45'>Danmark (+45)</option>\
+					</select>\
+					<div id='small-source'>Word source:</div>\
+				</div>\
+			</div>\
+			<div>\
+	  		<div id='source-info'>Take it easy with a savory helping of meaty deliciousness. &nbsp;Delicious words, that is.</div>\
+	  	</div><br/><br/>\
+			<div class='btn-group btn-group-main' data-toggle='buttons'>\
+				<label class='btn btn-success btn-diff active'><input type='radio' name='options' id='easy' autocomplete='off' checked> Easy </label>\
+	  		<label class='btn btn-warning btn-diff'><input type='radio' name='options' id='hard' autocomplete='off'> Hard </label>\
+	  		<label class='btn btn-danger btn-diff'><input type='radio' name='options' id='insane' autocomplete='off'> Insane </label>\
+	  	</div><br/></br>\
+	  	<div>\
+	  		<button id='startGame' class='btn btn-primary'>Start</button>\
+	  	<div>";
+
+	  $("#thanks").fadeOut();
+		$("#output").addClass("flex").html($("<div>").addClass("text-center").attr("id", "chooseGame").html(template).fadeIn());
+	}},
+
+	"loginComplete": { value: function() {
+		$("#sign-in").fadeOut(function() { game.chooseOptions(); });
+		$("#user-id").html(user.name || "");
+		if (user.photoURL) { $("#user-img").append($("<img>").attr("src", user.photoURL)); }
+	}},
+
+	"changeTheme": { value: function(){
+		const info = {
+			easy: "Take it easy with a savory helping of meaty deliciousness. &nbsp;Delicious words, that is.",
+			hard: "Retro at its finest? &nbsp;Meh, at least you can #humblebrag about your high score.",
+			insane: "Words straight from the depths of hell. &nbsp;If you like that sort of thing, we won't judge."
+		};
+
+		if ($("#bg").length) {
+			$("#source-text").velocity({ opacity: 0 }, { complete: function() { $("#source-text").html(game.currentSource).velocity({ opacity: 1 }) }});
+			$("#source-info").velocity({ opacity: 0 }, { complete: function() { $("#source-info").html(info[game.currentDifficulty]).velocity({ opacity: 1 }) }});
+		}
+
+		$(document.body).css("background-image", "none").velocity({ backgroundColor: "#cccccc"}, {duration: 800});
+		switch (game.currentDifficulty) {
+			case "easy":
+				if ($("#bg").length) { $("#bg").remove(); }
+				$(document.body).prepend($("<div>").attr("id", "bg").css("background-image", "url(assets/images/bacon.jpg)"));
+				break;
+			case "hard":
+				if ($("#bg").length) { $("#bg").remove(); }
+				$(document.body).prepend($("<div>").attr("id", "bg").css("background-image", "url(assets/images/hipster.jpg)"));
+				break;
+			case "insane":
+				if ($("#bg").length) { $("#bg").remove(); }
+				$(document.body).prepend($("<div>").attr("id", "bg").css("background-image", "url(assets/images/insane.jpg)"));
+				break;
+		}
+
+		$("#title").css("color", "black");
+	}},
+
+	"addWord": { value: function(word) {
+		// Create and output series of spans for each character in the word
+		let html = "<div id='word_" + word.number + "' class='falling-word'><div id='word_" + word.number + "_wrapper'><h2>";
+		for (let i = 0; i < word.str.length; i++) {
+		  html += "<span id='word_" + word.number + "_letter_" + i + "'>" + word.str[i] + "</span>";
+		}
+		html += "</h2></div></div>";
+		$("#output").append(html);
+
+		// Randomize y position of new word
+		const maxXPos = $("#game-body").width() - $("#word_" + word.number).width();
+		const rand = Math.floor(Math.random() * maxXPos) + 15;
+		$("#word_" + word.number).css("left", rand);
+
+		// Start animation, with callback to destroy word
+		$("#word_" + word.number).velocity({ translateY: "540px" },
+		  { duration: (word.str.length + 1) * game.wordSpeed, easing: "linear", complete: function() { if (game.activeWords.indexOf(word) != -1) { game.removeWord(word); } } });
+	}},
+
+	"removeWord": { value: function(word) {
+		// Remove word from HTML
+		$("#word_" + word.number).remove();
+
+		// Flash game panel
+		if (display.flashOn) {
+			$("#game-panel").css("box-shadow", "0px 0px 10px 1px red");
+			setTimeout(function(){ $("#game-panel").css("box-shadow", ""); }, 50);
+			setTimeout(function(){ $("#game-panel").css("box-shadow", "0px 0px 10px 1px red"); }, 120);
+			setTimeout(function(){ $("#game-panel").css("box-shadow", ""); }, 170);
+		}
+	}},
+
+	"updateWord": { value: function(word) {
+		$("#word_" + word.number + "_letter_" + game.currentLetter).css("color", "red");
+		if (word.str[game.currentLetter] === " ") { $("#word_" + word.number + "_letter_" + game.currentLetter).html("_"); }
+	}},
+
+	"resetWord": { value: function(word) {
+		for (let i = 0; i < game.currentLetter; i++) {
+			$("#word_" + word.number + "_letter_" + i).css("color", "black");
+			if ($("#word_" + word.number + "_letter_" + i).html() === "_") { $("#word_" + word.number + "_letter_" + i).html(" "); }
+		}
+	}},
+
+	"blowUpWord": { value: function(word) {
+		const absPosOffset = $("#word_" + word.number + "_letter_" + 0).width();
+
+		const horizSize = 120 + (8 * word.str.length);  // Horizontal explosion size
+		const horizRandMult = 40;  // Horizontal randomness
+		function horizOffset(index) { return -(horizSize / 2) + (index * (horizSize / (word.str.length - 1))) }
+		
+		const vertSize = 60 + (4 * word.str.length);  // Vertical explosion size
+		const vertRandMult = 24;  // Vertical randomness
+		let flip = Math.random() > 0.5 ? 1 : -1;
+		function vertOffset(index) { return Math.sin(index * Math.PI / (word.str.length - 1)) * vertSize * (flip *= -1)}
+		
+		const explodeTime = 250;  // Looks better if you keep the explode time at least 50% longer than the fade time
+		const fadeTime = 160;
+		
+		let horizRand;
+		let vertRand;
+
+		// Stop all running animations first
+		$("#word_" + word.number).velocity("stop", true);
+
+		for (let i = 0; i < word.str.length; i++) {
+			// Remove added dashes
+			if ($("#word_" + word.number + "_letter_" + i).html() === "_") { $("#word_" + word.number + "_letter_" + i).html(" "); }
+			
+			// Set all letters to absolute position and offset left to correct initial position
+			$("#word_" + word.number + "_letter_" + i).css("position", "absolute").css("left", (absPosOffset * i) + "px");
+
+			// Set random variation for final letter positions
+			horizRand = Math.random() * horizRandMult - (horizRandMult / 2);
+			vertRand = Math.random() * vertRandMult - (vertRandMult / 2);
+
+			// Animate explosion
+			$("#word_" + word.number + "_letter_" + i).velocity({
+				translateX: horizOffset(i) + horizRand,
+				translateY: vertOffset(i) + vertRand,
+			}, { duration: explodeTime, easing: "easeOutQuad"});
+		}
+
+		// Fade out word, then delete
+		$("#word_" + word.number).velocity("fadeOut", { duration: fadeTime, easing: "linear",
+			completion: function() { $("#word_" + word.number).remove() } });
+	}},
+
+	"shakeWord": { value: function(word) {
+		$("#word_" + word.number + "_wrapper").addClass("word-shake");
+		setTimeout(function(){ $("#word_" + word.number + "_wrapper").removeClass("word-shake"); }, display.shakeTime);
+	}},
+
+	"stats": { value: function() {
+		$("#lives").html(game.lives);
+		$("#score").html(stats.score);
+		$("#score-mult").html(stats.streakMultiplier > 1 ? "x" + stats.streakMultiplier : "");
+		$("#score-diff").html($("<span>").html((stats.scoreDelta > 0 ? ("+" + stats.scoreDelta) : "")).css("color", "#22f722").fadeIn().fadeOut());
+		$("#wpm").html(stats.wpm.toFixed(1));
+		$("#acc").html(stats.acc.toFixed(1) + "%");
+		$("#streak").html(stats.currentStreak);
+		$("#longest").html(stats.longestStreak);
+	}},
+
+	"highScores": { value: function(arr, index) {
+		$("#highscore-stats").empty();
+		
+		for (let i = 0; i < arr.length; i++) {
+			const template = "\
+				<th class='text-center hiscore-text'>" + (arr[i].score) + "</th>\
+				<th class='text-center hiscore-text'>" + arr[i].wpm.toFixed(1) + "</th>\
+				<th class='text-center hiscore-text'>" + arr[i].hits + " / " + (arr[i].hits + arr[i].misses) + " ( " + arr[i].acc.toFixed(1) + "% )</th>\
+				<th class='text-center hiscore-text'>" + arr[i].longestStreak + "</th>";
+
+			if (index !== null) {
+				$("#highscore-stats").append($("<tr>").html(template).fadeIn( index === i ? 400 : 0 ));
+			} else {
+				setTimeout(function() { $("#highscore-stats").append($("<tr>").html(template).fadeIn()) }, i * display.hiScoreTime);
+			}
+		}
+	}}
+});
+
+Object.seal(display);
---
+++ assets/javascript/game.js
@@ -0,0 +1,365 @@
+"use strict";
+
+// Main game object declaration
+const game = {
+	// Game state vars
+	currentLetter: 0,  // Current matching letter for words remaining in matchingWords
+	currentWord: 0,  // Used only to give a unique identifier to each word
+	currentTimeout: 0,  // Time to next word
+	currentSource: "",  // Currently selected word source
+	currentDifficulty: "easy",  // Current game difficulty
+	ready: false,
+	over: false,
+
+	// Word arrays
+	sourceWords: [],  // Source for adding words to game
+	activeWords: [],  // Words on screen
+	matchingWords: [],  // Words still matching input string
+	missedWords: [],  // Words that do not match input string (on current keystroke)
+
+	// Difficulty vars
+	lives: 5,
+	extraLifeAt: 10,  // Number of words completed to get another life
+	startingTimeout: 2100,  // Msec before first new word is added
+	minTimeout: 1200,  // Minimum msec between new words being added
+	maxWords: 6,  // Most words to be shown on screen at one time
+	wordSpeed: 1000,  // Speed of each word relative to its size, smaller numbers are faster
+
+	// Constants
+	speedupFactor: 1.001,  // Muliplier for the rate at which new words are added, applies after each added word
+	wordBuffer: 10,  // Minimum buffer of source words (will trigger an XHR more if sourceWords.length < wordBuffer)
+	missTimeout: 180,  // Msec after which a miss is triggered where new input is not allowed (like iFrames)
+
+
+	// Methods
+	init: function(){},
+		// Initializes game - makes all necessary queries for data (XHR or Firebase db) and creates main event listeners
+		// Calls: data.get
+		// Sets: (none)
+	
+	start: function(){},
+		// Start adding words to output div on timer (see // Constants for timing vars)
+		// Calls: stats.reset, addWord, end, stats.update
+		// Sets: currentLetter, currentWord, stats.startTime, ready, over
+
+	end: function(){},
+		// Sets game over state, sets up for next game, stores new high score
+		// Calls: display.gameOver, chooseOptions, stats.addHighScore
+		// Sets: ready, over
+
+	addWord: function(){},
+		// Creates a new word, calls data.get if sourceWords is running low
+		// Calls: data.get, display.addWord
+		// Sets: stats.timeOffset, stats.emptyStart, sourceWords, activeWords, matchingWords, currentWord
+
+	removeWord: function(word){},
+		// Word not completed in time - removes passed word from game, updates game state & stats
+		// Calls: stats.update, display.removeWord
+		// Sets: stats.score, stats.scoreDelta, stats.streakMultiplier, stats.currentStreak, activeWords, matchingWords, currentLetter
+
+	resetWord: function(word, index){},
+		// Resets previously matching word to initial state
+		// Calls: display.resetWord
+		// Sets: missingWords, matchingWords
+
+	completeWord: function(word){},
+		// Word successfully completed - removes passed word from game, updates game state & stats
+		// Calls: stats.update, display.blowUpWord
+		// Sets: stats.wordsCompleted, lives, stats.score, stats.scoreDelta, stats.streakMultiplier, stats.hits, stats.currentStreak, activeWords, matchingWords, stats.emptyStart, display.blowUpWord, end
+
+	wrongKey: function(){},
+		// Animates any words remaining in matchingWords when current keystroke results in no word matches, updates game state & stats
+		// Calls: stats.update, display.shakeWord
+		// Sets: stats.timeOffset, stats.scoreDelta, stats.streakMultiplier, ready, stats.hits, stats.misses, stats.currentStreak, stats.currentLetter
+
+	chooseOptions: function(){}
+		// Provides option selection for new game, starts new game with chosen options
+		// Calls: display.showOptions, display.startGame
+		// Sets: over, currentSource, sourceWords, currentDifficulty, length, startingTimeout, minTimeout, maxWords, wordSpeed, stats.difficultyMultiplier, stats.scoreMinusMult
+};
+
+
+// Method definitions
+Object.defineProperties(game, {
+	"init": { value: function() {
+		data.get("all");
+
+		// User sign-in popup
+		$("#auth").on('click', user.auth);
+
+		// Skip sign-in
+		$("#bypass").on('click', display.loginComplete);
+
+		// Main input processing function (on any keypress)
+		$(document).on("keypress", function(e) {
+			// Prevent spacebar from scrolling the page
+			if (String.fromCharCode(e.which) === " ") { e.preventDefault(); }
+
+			if (e.which === 13) {
+				while (game.matchingWords.length > 0) {
+					game.resetWord(game.matchingWords[0], 0);
+				}
+				game.currentLetter = 0;
+			// If game is ready for input and keystroke is on the allowed list
+			} else if (game.ready && /./.test(String.fromCharCode(e.which))) {
+				game.missedWords = [];  // Keeps an array of all remaining possible matches that miss this cycle
+
+				// If matchingWords is empty, copy all words from activeWords
+				if (game.matchingWords.length === 0) { game.matchingWords = game.activeWords.slice(); }
+
+				// Check all remaining words in checkingWords for matches
+				for (let i = 0; i < game.matchingWords.length; i++) {
+					const word = game.matchingWords[i];
+
+					// If the latest keystroke matches the current letter, make it red, and replace spaces with underlines
+					if (word.str[game.currentLetter] === String.fromCharCode(e.which)) {
+						display.updateWord(word);
+					} else {
+						game.resetWord(word, i);
+						i--;  // Decrement loop counter (the current word has been removed from matchingWords: next word was @ i+1, now @ i)
+					}
+				}
+
+				// If a match is found, increment currentLetter
+				if (game.matchingWords.length > 0) {
+					game.currentLetter++;
+					audio.play("hit");
+
+					// If word is complete, replace with new word
+					for (let i = 0; i < game.matchingWords.length; i++) {
+						if (game.currentLetter === game.matchingWords[i].str.length) {
+							game.completeWord(game.matchingWords[i]);
+							i--;  // Decrement loop counter (the current word has been removed from matchingWords: next word was @ i+1, now @ i)
+						}
+					}
+					if (game.matchingWords.length === 0) { game.currentLetter = 0; }
+				}
+				else {
+					game.wrongKey();
+				}
+			}
+		});
+
+		// High score radio button event
+		$("input[type=radio][name=scoreList]").on('change', function() {
+			display.highScores(stats[$(this).val() + "ScoreArr"], -1);
+		});
+	}},
+
+	"start": { value: function() {
+		// Game state & stats reset for new game
+		game.currentLetter = 0;
+		game.currentWord = 0;
+		stats.reset();
+		
+		let timer = setTimeout(anotherWord, 0);
+		game.currentTimeout = game.startingTimeout;
+
+		// Adds new word every time timer's callback function executes
+		// Reduces currentTimeout by speedupFactor on each new added word
+		function anotherWord() {
+		    clearTimeout(timer);
+		    if (game.lives > 0) {
+			    if (game.activeWords.length < game.maxWords) {
+			    	game.addWord();
+			    	game.currentTimeout / game.speedupFactor < game.minTimeout ? game.currentTimeout = game.minTimeout : game.currentTimeout /= game.speedupFactor;
+			    }
+			    timer = setTimeout(anotherWord, game.currentTimeout);
+			}
+		}
+
+		stats.startTime = Date.now();
+		stats.update();
+		game.over = false;
+		game.ready = true;
+	}},
+
+	"end": { value: function() {
+		audio.play("over");
+
+		game.over = true;
+		game.ready = false;
+		game.activeWords = [];
+		game.matchingWords = [];
+		
+		stats.duration = (Date.now() - stats.startTime) / 1000;
+		stats.addHighScore();
+		display.gameOver();
+
+		$("#playAgain").on('click', function() {
+			game.chooseOptions();
+		});
+	}},
+
+	"addWord": { value: function() {
+		// If no words were available to type, add the lost time to timeOffset
+		if (stats.emptyStart > 0) {
+			stats.timeOffset += Date.now() - stats.emptyStart;
+			stats.emptyStart = 0;
+		}
+
+		// Create new word object, remove word from sourceWords 
+		const word = {
+			str: game.sourceWords[0],
+			number: game.currentWord  // Stores the number used in the div and span IDs at HTML creation time
+		};
+		game.sourceWords.splice(0, 1);
+
+		// Get more words if sourceWords is running low
+		if (game.sourceWords.length < game.wordBuffer) { game.sourceWords = data.get(game.currentSource); }
+
+		// Show word on screen
+		display.addWord(word);
+
+		game.activeWords.push(word);
+		game.currentWord++;
+	}},
+
+	"removeWord": { value: function(word) {
+		game.lives--;
+		if (game.lives > 0) { audio.play("remove"); }
+
+		stats.scoreDelta = 0;
+		if (stats.streakMultiplier > 1) { stats.streakMultiplier--; }
+		stats.currentStreak = 0;
+		stats.update();
+
+		// Remove word from activeWords and matchingWords
+		game.activeWords.splice(game.activeWords.indexOf(word), 1)
+		if (game.matchingWords.indexOf(word) != -1) {
+			if (game.matchingWords.length === 1) { game.currentLetter = 0; }  // If the word still matches the current input, currentLetter = 0 (so new words will match)
+			game.matchingWords.splice(game.matchingWords.indexOf(word), 1);
+		}
+
+		// Remove word from display and trigger associated animations
+		display.removeWord(word);
+
+		// If game over conditions hold, trigger game over
+		if (game.lives <= 0 && !game.over) { game.end(); }
+	}},
+
+	"resetWord": { value: function(word, index) {
+		display.resetWord(word);
+		game.missedWords.push(word);
+		game.matchingWords.splice(index, 1);
+	}},
+
+	"completeWord": { value: function(word) {
+		audio.play("complete");
+
+		// Add an extra life for every bonusLifeAt words completed
+		stats.wordsCompleted++;
+		if (stats.wordsCompleted % game.extraLifeAt == 0 && game.lives < 10) { game.lives++; }
+
+		// Increment score, update stats, reset game vars
+		stats.score += word.str.length * stats.difficultyMultiplier * stats.streakMultiplier;
+		stats.scoreDelta = word.str.length * stats.difficultyMultiplier * stats.streakMultiplier;
+		stats.hits += word.str.length;
+		stats.currentStreak++;
+		if (word.str.length > stats.longestWord.length) { stats.longestWord = word.str; }
+		stats.update();
+
+		game.activeWords.splice(game.activeWords.indexOf(word), 1);
+		game.matchingWords.splice(game.matchingWords.indexOf(word), 1);
+
+		// "Blow up word" animation
+		display.blowUpWord(word);
+
+		// Timer check if screen is empty
+		if (game.activeWords.length === 0) {
+			stats.emptyStart = Date.now();
+		}
+
+		if (game.lives <= 0 && !game.over) { game.end(); }
+	}},
+
+	"wrongKey": { value: function() {
+		// Prohibits further input while animation is playing
+		game.ready = false;
+		
+		stats.scoreDelta = 0;
+		if (stats.streakMultiplier > 1) { stats.streakMultiplier--; }
+		stats.hits += game.currentLetter;
+		stats.misses++;
+		stats.currentStreak = 0;
+		stats.timeOffset += game.missTimeout;  // Removes forced wait time from WPM calculation (so you're not penalized for time spent waiting)
+		stats.update();
+
+		game.currentLetter = 0;
+
+		// Missed word animation
+		for (let i = 0; i < game.missedWords.length; i++) {
+			display.shakeWord(game.missedWords[i]);
+		}
+
+		setTimeout(function(){ game.ready = true; }, game.missTimeout);
+	}},
+
+	"chooseOptions": { value: function() {
+		display.showOptions();
+
+		easy();
+		display.changeTheme();
+		audio.play("intro");
+
+		$("input[type=radio][name=options]").on('change', function() {
+			audio.pause("intro");
+
+			if ($("#easy").prop("checked")) { easy(); }
+			else if ($("#hard").prop("checked")) { hard(); }
+			else { insane(); }
+			
+			display.changeTheme();
+			audio.play("intro");
+		});
+
+		$("#startGame").on("click", function(e){
+			if (true) {
+				game.over = false;
+
+				// Set source to difficulty word source, get words from that source
+				game.sourceWords = data.get(game.currentSource);
+
+				display.startGame();
+			}
+		});
+
+		function easy() {
+			game.currentSource = "custom";
+			game.currentDifficulty = "easy";
+			game.lives = 50;
+			game.startingTimeout = 3000;
+			game.minTimeout = 1800;
+			game.maxWords = 4;
+			game.wordSpeed = 3500;
+
+			stats.difficultyMultiplier = 100;
+		}
+
+		function hard() {
+			game.currentSource = "hipster";
+			game.currentDifficulty = "hard";
+			game.lives = 5;
+			game.startingTimeout = 2500;
+			game.minTimeout = 1500;
+			game.maxWords = 5;
+			game.wordSpeed = 1000;
+
+			stats.difficultyMultiplier = 200;
+		}
+
+		function insane() {
+			game.currentSource = "random";
+			game.currentDifficulty = "insane";
+			game.lives = 5;
+			game.startingTimeout = 2100;
+			game.minTimeout = 1000;
+			game.maxWords = 6;
+			game.wordSpeed = 600;
+
+			stats.difficultyMultiplier = 300;
+		}
+	}}
+});
+
+Object.seal(game);
